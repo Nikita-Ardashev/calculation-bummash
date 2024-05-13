7f3a1e08c7306272dc751f745f43b1c66b94a56b
Nikita-Ardashev <14214768-Nikita-Ardashev@users.noreply.gitlab.com>
2024-05-14 00:21:16 +0400

    Revert "asp v 7.0.18"
    This reverts commit 7d105aa84bc47477c54b0d98f017ad2fee5ba40f.

--- calculation-bummash.Server/obj/Debug/net8.0/calculation-bummash.Server.AssemblyInfo.cs
+++ calculation-bummash.Server/obj/Debug/net8.0/calculation-bummash.Server.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("calculation-bummash.Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d48cb0ad66287f900aff2efa40241812618238c2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+75c928da0ad2018ca8594904c53eb9b4d398c6ce")]
 [assembly: System.Reflection.AssemblyProductAttribute("calculation-bummash.Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("calculation-bummash.Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
